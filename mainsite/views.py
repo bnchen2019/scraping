e56b98416f568ddfeaa1a921ff237a48f934dee0
polinchen <ben.chen2019@gmail.com>
2021-06-20 03:09:54 +0000

    use template index.html to render view

--- mainsite/views.py
+++ mainsite/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
+from datetime import datetime
 
 # Create your views here.
 
 def homepage(request):
     posts = Post.objects.all()
-    post_lists = list()
-    for count, post in enumerate(posts):
-        post_lists.append("No.{}:".format(str(count)) + str(post)+ "<br>")
-    return HttpResponse(post_lists)
+    #post_lists = list()
+    now = datetime.now()
+    #for count, post in enumerate(posts):
+    #    post_lists.append("No.{}:".format(str(count)) + str(post)+ "<br>")
+    #return HttpResponse(post_lists)
+    return render(request, 'index.html', locals())
